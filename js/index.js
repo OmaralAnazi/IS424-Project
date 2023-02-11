5f0d9c9f637822d8f413310f31481b47dfcf77bf
Omar Alanazi <omkh790@gmail.com>
2023-02-11 23:11:52 +0300

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function renderPage() {
-    const bodyContent = document.body.innerHTML
+    const bodyContent = document.body.innerHTML;
     document.body.innerHTML = `    
         <header>
             <nav>
@@ -19,7 +19,27 @@ function renderPage() {
         <div class="footer">
             <p class="copyright">© 2023 IS424 Project.</p>
         <div>
-    `
+    `;
+}
+
+function sendEmail(){
+    var fullName = document.getElementById("first-name").value + " " + document.getElementById("last-name").value;
+    var age = document.getElementById("age").value;
+    var coutnry = document.getElementById("country").value;
+    var message = document.getElementById("message").value;
+    
+    if(fullName !== "" && message !== ""){
+        window.location.href = 
+`mailto:randomEmail@mail.com?subject=IS424&body=
+Information of The User: %0D%0A
+Full Name: ${fullName} %0D%0A
+Country: ${coutnry} %0D%0A
+Age: ${age} %0D%0A %0D%0A
+
+Here's The Message of The User: %0D%0A
+${message} %0D%0A %0D%0A`;
+    }
+
 }
 
 renderPage()
